9ce2dc2b551ef99209f6308a36d70520e4be5663
woo <woo114515@gmail.com>
2026-02-03 22:35:53 +0800

    docs: remove developing files

--- LinkList_test.py
+++
@@ -1,4 +0,0 @@
-from LinkList import LinkList,nil
-l = LinkList(1,LinkList(2,nil))
-s = LinkList(3,LinkList(2,nil))
-print(LinkList(l,LinkList(s,nil)))
--- interact_demo.py
+++
@@ -1,2 +0,0 @@
-from interact import *
-interact_mode()
